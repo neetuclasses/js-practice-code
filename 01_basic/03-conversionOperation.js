1a5668e21c3e0c0a58ea63ba4d63f2f4949d1c5b
neetuclasses <neetukanoujiya45@gmail.com>
2026-06-28 11:44:06 +0000

    conversionOperatipon

---
+++ 01_basic/03-conversionOperation.js
@@ -0,0 +1,26 @@
+let  score = undefined
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof score);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn );
+
+// "" => FALSE , "NEETU" => TRUE
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
