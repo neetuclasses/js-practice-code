c828d079ee4943567f67a9d818e9dbe1d09d63f4
neetuclasses <neetukanoujiya45@gmail.com>
2026-06-28 11:43:29 +0000

    datatypes

---
+++ 01_basic/02-datatypes.js
@@ -0,0 +1,22 @@
+//for documention use mdn & tc39.es //
+ 
+"use strict"; // treat all JS code as newer version
+
+//alert("3+3")// we are using nodejs, not browser
+
+let name = "neetu"  // String
+let age =  20       // number
+let isLooggedIn = false // boolean
+
+// number => 2 to power 53
+// bigint ( When we use if we have large number  , like trading , stock then we use)
+// string => ""
+// boolean => true/false
+// null =>  standalone value 
+// undefined => 
+// symbol => unique
+
+//  object
+
+// console.log(typeof undefines); undefined
+// console.log(typeof null); object
