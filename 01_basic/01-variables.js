2bb2f05062e31748f8469253474dd420ff833952
neetuclasses <neetukanoujiya45@gmail.com>
2026-06-28 11:42:54 +0000

    variables

---
+++ 01_basic/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 1144553
+let accountEmail = "neetu@goggle.com"
+var accountPassword = "12345"
+accountCity ="Delhi"
+
+//never use var
+
+accountEmail = "nitu@goggle.com"
+accountPassword = "4321"
+accountCity = "Goa"
+
+console.log("accountId");
+
+console.table([ accountId,accountEmail,accountPassword,accountCity])
