f66ce2e537aa6bf57c5ad7d460aa5fc314b180aa
neetuclasses <neetukanoujiya45@gmail.com>
2026-06-30 14:42:59 +0000

    conversion

--- 01_basic/03-conversionOperation.js
+++ 01_basic/03-conversionOperation.js
@@ -21,6 +21,26 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
 
+// *******************************Operation**********************************
+
+let value = 33
+let nagValue = - value
+console.log(nagValue);
+
+
+let str1 = " Hello"
+let str2 = " Neetu"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter =  100 
+++gameCounter;
+console.log(gameCounter)
